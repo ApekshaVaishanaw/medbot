928b688b5ad918c91fa11fbba9f3b93416173026
ApekshaVaishanaw <114852773+ApekshaVaishanaw@users.noreply.github.com>
2024-09-28 11:32:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= '',
+    author_email='',
     packages= find_packages(),
     install_requires = []
 
